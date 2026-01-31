58023fa6cce8219aace63893a8c8cc417e820833
Rachiͤt Takate <49189777+Bluejet007@users.noreply.github.com>
2026-01-31 10:30:14 +0530

    Add week 4

---
+++ Week4/Q2.py
@@ -0,0 +1,98 @@
+import torch as T
+import matplotlib.pyplot as plt
+from torch.utils.data import Dataset, DataLoader
+import torch.nn as nn
+
+T.manual_seed(7)
+X = T.tensor([
+    [0.0, 0.0],
+    [0.0, 1.0],
+    [1.0, 0.0],
+    [1.0, 1.0]]) 
+Y = T.tensor([0.0, 1.0, 1.0, 0.0])
+dev = T.device('cuda' if T.cuda.is_available() else 'cpu')
+batch = 1
+eps = 1000
+lr = 0.05
+
+class MyDataset(Dataset):
+    def __init__(self, X, Y):
+        self.X = X
+        self.Y = Y
+
+    def __len__(self):
+        return len(self.X)
+    
+    def __getitem__(self, idx):
+        return self.X[idx].to(dev), self.Y[idx].to(dev)
+
+class XORModel(nn.Module):
+    def __init__(self, ):
+        super().__init__()
+
+        self.lin1 = nn.Linear(2, 2, dtype=T.float32)
+        self.act1 = nn.ReLU()
+        self.lin2 = nn.Linear(2, 1)
+        self.act2 = nn.ReLU()
+
+    def forward(self, x: T.Tensor):
+        x = self.lin1(x)
+        x = self.act1(x)
+        x = self.lin2(x)
+        return self.act2(x)
+
+
+full_data = MyDataset(X, Y)
+data_load = DataLoader(full_data, batch_size=batch, shuffle=True)
+mod = XORModel().to(dev)
+print(mod)
+loss_fn = nn.BCEWithLogitsLoss()
+opter = T.optim.SGD(mod.parameters(), lr=lr)
+
+def one_epoch():
+    total = 0.0
+    for x, y in data_load:
+        opter.zero_grad()
+        p = mod(x)
+
+        loss = loss_fn(p.flatten(), y)
+        loss.backward()
+        opter.step()
+
+        total += loss.item()
+
+    return total / len(data_load)
+
+print()
+loss_l = []
+for e in range(eps):
+    mod.train(True)
+
+    total = 0.0
+    for x, y in data_load:
+        opter.zero_grad()
+        p = mod(x)
+
+        loss = loss_fn(p.flatten(), y)
+        loss.backward()
+        opter.step()
+
+        total += loss.item()
+
+    avg_loss = total / (len(data_load) * batch)
+    loss_l.append(avg_loss)
+
+    if e % (eps // 10) == 0:
+        print(f'EP {e}/{eps}, loss: {avg_loss}')
+
+print()
+for param in mod.named_parameters():
+    print(param)
+
+inp = T.tensor([0.0, 1.0], device=dev)
+mod.eval()
+print(f'\nFor x = {inp}, p = {T.sigmoid(mod(inp))}')
+
+plt.plot(loss_l)
+plt.title('Log-loss vs Epochs')
+plt.show()
