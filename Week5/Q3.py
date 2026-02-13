6e4310096c650b65810d89ff4d96298a16ffca64
Bluejet007 <rachit.takate@gmail.com>
2026-02-14 01:29:56 +0530

    Add confusion matrix to week 5

--- Week5/Q3.py
+++ Week5/Q3.py
@@ -1,11 +1,11 @@
-import torch
-import torch.nn as nn
+import torch as T
+from torch import nn
 from torch.utils.data import DataLoader
 from torchvision.datasets import MNIST
 import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
 
-dev = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+dev = T.device('cuda' if T.cuda.is_available() else 'cpu')
 print('Using device:', dev)
 
 batch_size = 64
@@ -17,14 +17,11 @@ transform = transforms.Compose([
     transforms.Normalize((0.1307,), (0.3081,))
 ])
 
-train_dataset = MNIST('./data', train=True, download=True, transform=transform)
+train_dataset = MNIST('./data', transform=transform, download=True)
+test_dataset = MNIST('./data', False, transform, download=True)
 
-train_loader = DataLoader(
-    train_dataset,
-    batch_size=batch_size,
-    shuffle=True,
-    pin_memory=True
-)
+train_loader = DataLoader(train_dataset, batch_size, True)
+test_loader = DataLoader(test_dataset, batch_size, True)
 
 class CNNClassifier(nn.Module):
     def __init__(self, red=1):
@@ -85,17 +82,37 @@ class Trainer:
             print(f'Epoch [{e+1}/{eps}], loss = {avg_loss}')
 
         return loss_l
+    
+    def conf_mat(self, loader, num_classes=10):
+        self.mod.eval()
+        cm = T.zeros(num_classes, num_classes, dtype=T.int64)
+
+        with T.no_grad():
+            for x, y in loader:
+                x = x.to(self.dev, non_blocking=True)
+                y = y.to(self.dev, non_blocking=True)
+
+                logits = self.mod(x)
+                preds = T.argmax(logits, dim=1)
+
+                for t, p in zip(y.view(-1), preds.view(-1)):
+                    cm[t.long(), p.long()] += 1
+
+        return cm
 
 reds = [1]
+cm = None
 for red in reds:
     mod = CNNClassifier()
     loss_fn = nn.CrossEntropyLoss()
-    opter = torch.optim.SGD(mod.parameters(), lr=lr)
+    opter = T.optim.SGD(mod.parameters(), lr=lr)
 
     trainer = Trainer(mod, opter, loss_fn, dev)
     losses = trainer.fit(train_loader, epochs)
+    cm = trainer.conf_mat(test_loader)
     plt.plot(losses)
 
+print(cm)
 plt.legend(reds)
 plt.xlabel('Epoch')
 plt.ylabel('Loss')
